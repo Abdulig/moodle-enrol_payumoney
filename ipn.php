c0b29c2aad8b67cc4fc90225bbb14bb51e2f6301
nikhildatta <51258151+nikhildatta@users.noreply.github.com>
2019-06-01 14:31:52 +0530

    Update ipn.php

--- ipn.php
+++ ipn.php
@@ -31,7 +31,10 @@ define('NO_DEBUG_DISPLAY', true);
 
 require("../../config.php");
 require_once("lib.php");
-require_once($CFG->libdir.'/eventslib.php');
+if($CFG->version < 2018101900)
+{
+    require_once($CFG->libdir.'/eventslib.php');
+}
 require_once($CFG->libdir.'/enrollib.php');
 require_once($CFG->libdir . '/filelib.php');
 
